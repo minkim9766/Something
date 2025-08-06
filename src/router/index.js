775837b70b45bb9238e0facf6c267e4973bfbf3a
minkim9766 <musicminseo9766@gmail.com>
2025-08-07 01:23:15 +0900

    fixed router and build2

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Homepage from '../views/HomeView.vue'
 import PostsPage from '../views/PostsPage.vue'
 
@@ -8,7 +8,7 @@ const routes = [
     ]
 
 const router = createRouter({
-history: createWebHistory(),
+history: createWebHashHistory(),
 routes
 })
 
