392bd05560d1a1cf1373a34916c4aacdc24decba
MinseoKim <155244854+minkim9766@users.noreply.github.com>
2025-08-07 11:55:48 +0900

    index.js 업데이트

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,14 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import Homepage from '../views/HomeView.vue'
 import PostsPage from '../views/PostsPage.vue'
 
-const routes = [
-  { path: '/', name: 'Homepage', component: Homepage },
-    { path: '/posts', name: 'Posts', component: PostsPage }
-    ]
-
+// GitHub Pages에서 사용하는 경우 반드시 base에 repo 이름 포함
 const router = createRouter({
-history: createWebHashHistory(),
-routes
+  history: createWebHistory('/something/'), // 예: '/my-vue-app/'
+  routes: [
+    { path: '/', name: 'Homepage', component: Homepage },
+    { path: '/posts', name: 'Posts', component: PostsPage }
+  ]
 })
 
 export default router
