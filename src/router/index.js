35dc450de0352f9d6aeab3bee8768651278cf08c
minkim9766 <musicminseo9766@gmail.com>
2025-08-08 16:36:09 +0900

    build5 - fix 404 page1

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ import PostsPage from '../views/PostsPage.vue'
 
 // GitHub Pages에서 사용하는 경우 반드시 base에 repo 이름 포함
 const router = createRouter({
-  history: createWebHistory('/something/'), // 예: '/my-vue-app/'
+  history: createWebHistory('/Something/'), // 예: '/my-vue-app/'
 routes: [
 { path: '/', name: 'Homepage', component: Homepage },
     { path: '/posts', name: 'Posts', component: PostsPage }
